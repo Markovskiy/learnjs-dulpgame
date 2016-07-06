4d76c25affd833f554db448feaef2bef65bdf3a7
Nicolai Voronin <naranjadito@gmail.com>
2016-07-06 16:13:12 +0300

    Добавил в модуль app.js класс игры и перенес часть управления в game.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,23 @@ import '../css/main.css';
 
 import Circle from './components/Circle.js';
 import Interface from './components/Interface.js';
+import Game from './components/game.js';
 
-const circle = new Circle({
-	name: '1',
-	colorCount: 5,
-	colorSlice: [45, 45, 90, 150, 30],
-	colors: ['#26C6DA', '#D4E157', '#FF7043', '#7E57C2', '#B2DFDB'],
-	circleSpeed: 1,
-	bulletSpeed: 1,
-});
+//const circle = new Circle({
+//  name: '1',
+//  colorCount: 5,
+//  colorSlice: [45, 45, 90, 150, 30],
+//  colors: ['#26C6DA', '#D4E157', '#FF7043', '#7E57C2', '#B2DFDB'],
+//  circleSpeed: 1,
+//  bulletSpeed: 1,
+//});
 
-const userInterface = new Interface();
+const game = new Game();
+//const userInterface = new Interface();
 
 const startButton = document.querySelector('.btn__start');
 
-circle.renderSlices();
-
-startButton.addEventListener('click', userInterface.showGameScreen.bind(userInterface));
+//circle.renderSlices();
+//game.initGame();
+//startButton.addEventListener('click', userInterface.showGameScreen.bind(userInterface));
+startButton.addEventListener('click', game.initGame.bind(game));
