d01e9887f1ea044436ee6d0d76fd3a432f65affe
Nicolai Voronin <naranjadito@gmail.com>
2016-07-06 16:11:09 +0300

    Сделал набросок компонента игры

--- src/js/components/game.js
+++ src/js/components/game.js
@@ -4,27 +4,63 @@
 // (function(argument) {
 //
 //
+import Circle from './Circle.js';
+import Interface from './Interface.js';
 
 /* *
 * @class Game class with main loop
 */
-class Game {
-/* *
-* @constructor
-*/
-	// constructor (level) {
+export default class Game {
+  /* *
+  * @constructor
+  */
+  constructor(level) {
+    this.level = level;
+    this.circleOptions = /* this.level.circle.options || */{
+      name: '1',
+      colorCount: 5,
+      colorSlice: [45, 45, 90, 150, 30],
+      colors: ['#26C6DA', '#D4E157', '#FF7043', '#7E57C2', '#B2DFDB'],
+      circleSpeed: 1,
+      bulletSpeed: 1
+      };
 
-	// }
+    this.userInterface = new Interface();
+    this.circle = new Circle(this.circleOptions );
 
-	/**
-	* Some method
-	* @return {number}
-	*/
-	someMethod() {
-		return Math.random();
-	}
+  }
+
+  initGame() {
+    this.render();
+    this.userInterface.showGameScreen();
+    //this.gameLoop();
+  }
+
+  render() {
+    this.circle.renderSlices();
+  }
+
+  gameLoop() {
+    debugger;
+    let time = Date.now();
+    let delta = time - this._lastTime; // сколько прошло с последнего обновления;
+    this._lastTime = time; // сохраним на следующий вызов текущее время;
+
+    this.circle.update(delta); // провернем круг исходя из прошедшего с последнего поворота времени
+    if (this._activeBullet) {
+      this.activeBullet.update(delta); // "продвинем» пулю, если она есть на  нужное расстояние.
+
+      this.checkIntersection(); // проверим, а не долетела ли пуля до круга
+    }
+
+    this._timerID = setInterval(this.updateState.bind(this), 11);
+  }
+
+  checkIntersection() {
+    return false;
+  }
 }
 
 // export
-window.Game = Game;
+//window.Game = Game;
 // })()
